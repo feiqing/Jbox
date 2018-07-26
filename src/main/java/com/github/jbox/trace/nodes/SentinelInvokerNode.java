95f9f9c876c487be2b3099f7bfd2d1ebb3da6831
jifang.zjf <jifang.zjf@alibaba-inc.com>
2018-07-26 22:24:28 +0800

    SentinelInvokerNode

--- src/main/java/com/github/jbox/trace/nodes/SentinelInvokerNode.java
+++ src/main/java/com/github/jbox/trace/nodes/SentinelInvokerNode.java
@@ -1,7 +1,18 @@
 package com.github.jbox.trace.nodes;
 
-import com.github.jbox.trace.NodeContext;
 import com.github.jbox.trace.InvokerNode;
+import com.github.jbox.trace.NodeContext;
+import com.github.jbox.trace.TraceException;
+import com.taobao.csp.sentinel.Entry;
+import com.taobao.csp.sentinel.SphU;
+import com.taobao.csp.sentinel.slots.block.BlockException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+import static com.github.jbox.utils.JboxUtils.getSimplifiedMethodName;
 
 /**
  * @author jifang.zjf@alibaba-inc.com (FeiQing)
@@ -10,8 +21,27 @@ import com.github.jbox.trace.InvokerNode;
  */
 public class SentinelInvokerNode implements InvokerNode {
 
+    private static final Logger rootLogger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+
     @Override
     public void invoke(NodeContext context) throws Throwable {
-        context.next();
+        Entry entry = null;
+        Method method = context.getMethod();
+        try {
+            if (!Modifier.isPrivate(method.getModifiers()) && !Modifier.isProtected(method.getModifiers())) {
+                entry = SphU.entry(method);
+                context.next();
+            } else {
+                context.next();
+            }
+        } catch (BlockException e) {
+            String msg = "method: [" + getSimplifiedMethodName(method) + "] invoke was blocked by sentinel.";
+            rootLogger.warn(msg, e);
+            throw new TraceException(msg, e);
+        } finally {
+            if (entry != null) {
+                entry.exit();
+            }
+        }
     }
 }
