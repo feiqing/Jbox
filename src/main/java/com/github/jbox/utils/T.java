1e7e9c489ba7c116241dfb5ac5635bb5947b2457
jifang.zjf <jifang.zjf@alibaba-inc.com>
2019-10-21 14:56:50 +0800

    fuck

--- src/main/java/com/github/jbox/utils/T.java
+++ src/main/java/com/github/jbox/utils/T.java
@@ -1,5 +1,7 @@
 package com.github.jbox.utils;
 
+import java.util.Calendar;
+
 /**
  * @author jifang.zjf@alibaba-inc.com (FeiQing)
  * @version 1.0
@@ -17,25 +19,37 @@ public class T {
 
     public static final int OneW = 7 * OneD;
 
-    public static Long ts(Long timestamp) {
-        return timestamp - timestamp % OneS;
+    public static long ts(long timestamp) {
+        return t(timestamp, Calendar.MILLISECOND);
     }
 
-    public static Long tm(Long timestamp) {
-        return timestamp - timestamp % OneM;
+    public static long tm(long timestamp) {
+        return t(timestamp, Calendar.SECOND, Calendar.MILLISECOND);
     }
 
-    public static Long th(Long timestamp) {
-        return timestamp - timestamp % OneH;
+    public static long th(long timestamp) {
+        return t(timestamp, Calendar.MINUTE, Calendar.SECOND, Calendar.MILLISECOND);
     }
 
-    public static long td(Long timestamp) {
-        // GMT+8
-        return timestamp - timestamp % OneD - 8 * OneH;
+    public static long td(long timestamp) {
+        return t(timestamp, Calendar.HOUR_OF_DAY, Calendar.MINUTE, Calendar.SECOND, Calendar.MILLISECOND);
+    }
+
+    private static long t(long ts, int... fields) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(ts);
+        for (int field : fields) {
+            calendar.set(field, 0);
+        }
+        return calendar.getTimeInMillis();
     }
 
 
     public static void main(String[] args) {
+        test1();
+    }
+
+    private static void test0() {
         long l = System.currentTimeMillis();
         System.out.println(DateUtils.timeMillisFormat(l));
         System.out.println(DateUtils.timeMillisFormat(ts(l)));
@@ -44,4 +58,12 @@ public class T {
         System.out.println(DateUtils.timeMillisFormat(td(l)));
     }
 
+    private static void test1() {
+        long l = System.currentTimeMillis() - 14 * T.OneH;
+        System.out.println(DateUtils.timeMillisFormat(l));
+        System.out.println(DateUtils.timeMillisFormat(ts(l)));
+        System.out.println(DateUtils.timeMillisFormat(tm(l)));
+        System.out.println(DateUtils.timeMillisFormat(th(l)));
+        System.out.println(DateUtils.timeMillisFormat(td(l)));
+    }
 }
