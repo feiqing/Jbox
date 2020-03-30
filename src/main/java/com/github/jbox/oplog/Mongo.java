856fb332f669071cad647b74886dd9ebbcc7b67a
jifang.zjf <jifang.zjf@alibaba-inc.com>
2020-03-30 17:19:39 +0800

    find bean by class

--- src/main/java/com/github/jbox/oplog/Mongo.java
+++ src/main/java/com/github/jbox/oplog/Mongo.java
@@ -2,13 +2,17 @@ package com.github.jbox.oplog;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @author jifang.zjf@alibaba-inc.com (FeiQing)
  * @version 1.0
  * @since 2019/10/24 9:29 PM.
  */
 @Data
-public class Mongo {
+public class Mongo implements Serializable {
+
+    private static final long serialVersionUID = -2081965221916801594L;
 
     private String url;
 
--- src/main/java/com/github/jbox/rpc/hessian/impl/RpcProcessorImpl.java
+++ src/main/java/com/github/jbox/rpc/hessian/impl/RpcProcessorImpl.java
@@ -6,18 +6,13 @@ import com.github.jbox.rpc.proto.RpcParam;
 import com.github.jbox.utils.IPv4;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
-import org.springframework.util.CollectionUtils;
-import org.springframework.util.ReflectionUtils;
 
 import java.io.Serializable;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.atomic.AtomicReference;
 
 import static com.github.jbox.utils.JboxUtils.runWithNewMdcContext;
+import static com.github.jbox.utils.RpcUtil.*;
 
 /**
  * @author jifang.zjf@alibaba-inc.com (FeiQing)
@@ -41,13 +36,13 @@ public class RpcProcessorImpl implements RpcProcessor {
             Serializable result = null;
             Throwable except = null;
             try {
-                Object bean = getBeanByClass(param.getClassName());
+                Object bean = getBeanByClass(param.getClassName(), applicationContext, log);
                 if (bean != null) {
                     result = invoke(bean, param.getMethodName(), param.getArgs());
                     return result;
                 }
 
-                bean = getBeanByName(param.getClassName());
+                bean = getBeanByName(param.getClassName(), applicationContext, log);
                 if (bean != null) {
                     result = invoke(bean, param.getMethodName(), param.getArgs());
                     return result;
@@ -78,93 +73,7 @@ public class RpcProcessorImpl implements RpcProcessor {
     }
 
     private Serializable invoke(Object bean, String method, Object[] args) throws InvocationTargetException, IllegalAccessException {
-        Method methodInstance = getMethod(bean.getClass(), method);
+        Method methodInstance = getMethod(bean.getClass(), method, log);
         return (Serializable) methodInstance.invoke(bean, args);
-    }
-
-    private static final ConcurrentMap<Class, Map<String, Method>> cache = new ConcurrentHashMap<>();
-
-    private Method getMethod(Class clazz, String methodName) {
-        Map<String, Method> methods = cache.get(clazz);
-
-        if (methods == null) {
-            Map<String, Method> methodMap = new ConcurrentHashMap<>();
-            for (Method method : ReflectionUtils.getAllDeclaredMethods(clazz)) {
-                methodMap.put(method.getName(), method);
-            }
-
-            cache.put(clazz, methodMap);
-            methods = methodMap;
-        }
-
-        Method method = methods.get(methodName);
-        if (method == null) {
-            log.error("can't find method:[{}] from class:[{}]", methodName, clazz);
-        }
-
-        return method;
-    }
-
-    private static final ConcurrentMap<String, AtomicReference<Object>> class2bean = new ConcurrentHashMap<>();
-
-    private Object getBeanByClass(String clazz) {
-        AtomicReference<Object> optional = class2bean.get(clazz);
-        if (optional != null) {
-            return optional.get();
-        }
-
-        Object bean = doGetBeanByClass(clazz);
-        class2bean.put(clazz, new AtomicReference<>(bean));
-
-        return bean;
-    }
-
-    private Object doGetBeanByClass(String clazz) {
-        try {
-            Map<String, ?> beans = applicationContext.getBeansOfType(Class.forName(clazz));
-            if (CollectionUtils.isEmpty(beans)) {
-                return null;
-            }
-
-            if (beans.size() == 1) {
-                return beans.values().iterator().next();
-            }
-
-            for (Object bean : beans.values()) {
-                if (bean.getClass().getName().equals(clazz)) {
-                    return bean;
-                }
-            }
-        } catch (Throwable t) {
-            log.error("doGetBeanByClass error:[{}]", clazz, t);
-        }
-
-        return null;
-    }
-
-    private static final ConcurrentMap<String, AtomicReference<Object>> name2bean = new ConcurrentHashMap<>();
-
-    private Object getBeanByName(String clazz) {
-        AtomicReference<Object> optional = name2bean.get(clazz);
-        if (optional != null) {
-            return optional.get();
-        }
-
-        Object bean = doGetBeanByName(clazz);
-        name2bean.put(clazz, new AtomicReference<>(bean));
-
-        return bean;
-    }
-
-    private Object doGetBeanByName(String clazz) {
-        String beanName = clazz.substring(clazz.lastIndexOf(".") + 1);
-        beanName = beanName.substring(0, 1).toLowerCase() + beanName.substring(1);
-        try {
-            return applicationContext.getBean(beanName);
-        } catch (Throwable t) {
-            log.error("doGetBeanByName error:[{}]", beanName, t);
-        }
-
-        return null;
     }
 }
